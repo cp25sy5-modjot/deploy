289dec76e1901ebcb70b0fbed3b0a81b915e92d5
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2026-01-12 22:54:01 +0700

    change init db to sh instead sql

---
+++ db/init/001-create-role-and-db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 <<-EOSQL
+DO \$\$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${POSTGRES_USER}') THEN
+    CREATE ROLE ${POSTGRES_USER}
+      LOGIN
+      PASSWORD '${POSTGRES_PASSWORD}';
+  END IF;
+END
+\$\$;
+
+DO \$\$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${POSTGRES_DB}') THEN
+    CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER};
+  END IF;
+END
+\$\$;
+EOSQL
