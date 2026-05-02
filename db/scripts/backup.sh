3018de9faea1e7bce2e35b210f236bb6dfedd620
Natthaphol Nirattisaikul <zerozkiller@gmail.com>
2026-05-02 19:59:07 +0700

    update

--- db/scripts/backup.sh
+++ db/scripts/backup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 DB_HOST="postgres-db"
-DB_NAME=${POSTGRES_DB}
-DB_USER=${POSTGRES_USER}
+DB_NAME=${POSTGRES_DB:-postgres}
+DB_USER=${POSTGRES_USER:-postgres}
 
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
@@ -11,7 +11,12 @@ echo "Running backup..."
 pg_dump -h $DB_HOST -U $DB_USER $DB_NAME \
   | gzip > /backup/backup_$TIMESTAMP.dump.gz
 
-echo "Backup done!"
+if [ $? -eq 0 ]; then
+  echo "Backup done!"
+else
+  echo "Backup failed ❌"
+  exit 1
+fi
 
 # ลบไฟล์เก่า
 find /backup -type f -mtime +7 -name "*.dump.gz" -delete
